8eca0f3286cfd52cbe36bd3f13b00f5b0acacc1b
Wissem Chihaoui <chihahoui10@gmail.com>
2025-02-11 18:08:08 +0100

    fix theme

--- src/components/settings/config-settings.js
+++ src/components/settings/config-settings.js
@@ -7,10 +7,10 @@ export const STORAGE_KEY = 'app-settings';
 export const defaultSettings = {
   colorScheme: 'light',
   direction: 'ltr',
-  contrast: 'default',
-  navLayout: 'vertical',
-  primaryColor: 'default',
-  navColor: 'integrate',
+  contrast: 'hight',
+  navLayout: 'horizontal',
+  primaryColor: 'blue',
+  navColor: 'apparent',
   compactLayout: true,
   fontFamily: defaultFont,
 };
